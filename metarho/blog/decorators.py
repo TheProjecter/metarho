d4b0ceb51ba7a82bf2693262b7b883856f925249
Streamweaver <streamweaver@mindspring.com>
2010-04-08 23:27:07 -0400

    Adding more decorators.

--- metarho/blog/decorators.py
+++ metarho/blog/decorators.py
@@ -4,6 +4,48 @@ from django.core.urlresolvers import reverse
 
 from metarho.blog.models import Post
 
+CONTENT_MAP = {
+    'application/json': ('metarho.blog.json', '_json'),
+    'text/xml': ('metarho.blog.feeds', '_rss'),
+    'application/rss+xml': ('metarho.blog.feeds', '_rss'),
+    'application/atom+xml': ('metarho.blog.feeds','_atom'),
+    'application/rdf+xml': ('metarho.blog.rdf', '_rdf')
+}
+
+FORMAT_MAP = {
+    'json': ('metarho.blog.json', '_json'),
+    'xml': ('metarho.blog.feeds', '_rss'),
+    'rss': ('metarho.blog.feeds', '_rss'),
+    'atom': ('metarho.blog.feeds','_atom'),
+    'rdf': ('metarho.blog.rdf', '_rdf')
+}
+
+def format_negotiation(view_fn):
+    '''
+    Checks the request for a querstring value called format and returns an
+    alernate response if the format type matches a method name of the
+    'view_fn' + 'format_map_type'.
+
+    for example if there the decorated view method was called 'post_list'
+    and the format attribue of the querystring was rss::
+
+       /posts/?format=rss
+
+    Then it would match a value in the FORMAT_MAP and return the method
+    'metarho.blog.feeds.post_list_rss instead.
+
+    '''
+
+    def decorator(request, *args, **kwargs):
+        format = request.GET.get('format', None)
+        if format and format in FORMAT_MAP:
+            True # @TODO use new module loader to get the proper modules.
+
+        # Default to returning the original method
+        return view_fn(request, *args, **kwargs)
+
+    return decorator
+
 def wp_post_redirect(view_fn):
     '''
     Checks a request for a querystring item matching a wordpress 
